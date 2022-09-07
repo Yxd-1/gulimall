c0dfdae63c71648f82e7ae158c8b56e3edcc4195
youxudong <1520501795@qq.com>
2022-09-07 22:17:18 +0800

    项目结构初始化

---
+++ mall-coupon/src/test/java/com/yxd/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxd.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-ware/src/test/java/com/yxd/mall/ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxd.mall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
